094384f844da80a2a0d222bdf5439449721babd7
NourrKhalled <nourrkhalled123@gmail.com>
2025-01-25 07:05:04 +0200

    Added Compose Component

--- pigeon-mail-frontend/src/PigeonMail.jsx
+++ pigeon-mail-frontend/src/PigeonMail.jsx
@@ -1,13 +1,21 @@
+import React, { useState } from 'react';
 import './PigeonMail.css'
 import Inbox from './inbox/Inbox'
+import Compose from './inbox/Compose'
+import {Drawer} from '@mui/material';
 
 function PigeonMail() {
+
+const [openInbox, setOpenInbox] = useState(false);
+const [openCompose, setOpenCompose] = useState(false);
 
   return (
     <>
     <div className='base'>
 
       <div className='sidebar'>
+              <button onClick={() => setOpenInbox(true)}>Inbox</button>
+              <button onClick={() => setOpenCompose(true)}>Compose</button>
 
       </div>
 
@@ -19,6 +27,7 @@ function PigeonMail() {
 
             <div className='main-body'>
               <Inbox />
+              {openCompose && <Compose onClose={() => setOpenCompose(false)}/>}
 
             </div>
 
---
+++ pigeon-mail-frontend/src/inbox/Compose.jsx
@@ -0,0 +1,37 @@
+import './Compose.css';
+import React from 'react';
+import Draggable from 'react-draggable';
+import { Box, TextField, Button } from '@mui/material';
+import { IoMdClose, IoMdAttach } from "react-icons/io";
+
+
+const Compose = ({ onClose }) => {
+
+  return (
+    <Draggable>
+      <Box id='compose-box'>
+        {/* Header */}
+        <Box id='compose-header'>
+          <span>New Message</span>
+          <Box id='icon-close'><IoMdClose onClick={onClose}/></Box>
+        </Box>
+
+        {/* Input */}
+        <Box id='compose-input'>
+            <TextField id='input' label="To"        type="email" size="small"/>
+            <TextField id='input' label="Subject"   type="text"  size="small"/>
+            <TextField id='input' label="Message"   multiline rows={8} />
+        </Box>
+        {/* Tools */}
+        <Box id='tools-box'>
+          <Box id='icon-attach'><IoMdAttach /></Box>
+          <Button variant="contained" onClick={() => alert('send processed')}>
+            Send
+          </Button>
+        </Box>
+      </Box>
+    </Draggable>
+  )
+}
+
+export default Compose;
